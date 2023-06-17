2d93c877d8fc8132a925efb1a2ec3fd78686df85
krzysckh <krzysckh@wp.pl>
2023-06-17 13:27:12 +0200

    animated example for plot

--- examples/plot1.scm
+++ examples/plot1.scm
@@ -1,37 +1,34 @@
-; plot of y = 1/x
+; animated plot o_O
 
+(use "core")
 (use "colors")
 (use "plot")
-(use "core")
 
 (define-background-color '(34 34 34))
 (define-font-color '(222 222 222))
 
 (define y-fn
   (lambda (x)
-    (if (= x 0)
-      0
-      (/ 1 x))))
-
-;(define y-fn
-  ;(lambda (x)
-    ;(* x x)))
+    (expt x 3)))
 
 (define opt (list-copy default-plot-options))
 
 (set-cdr! (assq 'line-color opt) (list '(222 222 222)))
 (set-cdr! (assq 'box-color opt) (list '(34 34 34)))
 
-(define draw-min -10)
-(define draw-max 10)
-
-(plot-set-xy
-  opt
-  (list
-    (flatten (range-stepped draw-min draw-max 1/20))
-    (flatten (map y-fn (range-stepped draw-min draw-max 1/20)))))
+(define draw-min -1)
+(define draw-max 1)
 
 (define update-screen
   (lambda ()
+    (plot-set-xy
+      opt
+      (list
+        (flatten (range-stepped draw-min draw-max 1/20))
+        (flatten (map y-fn (range-stepped draw-min draw-max 1/20)))))
+
+    (set! draw-min (- draw-min 1/20))
+    (set! draw-max (+ draw-max 1/20))
+
     (plot opt)))
 
