4c3bd1f5d6eae22567e6e7da288f888892063b2f
krzysckh <krzysckh@wp.pl>
2023-06-19 10:49:17 +0200

    make smth

--- scm/make.scm
+++ scm/make.scm
@@ -3,10 +3,23 @@
 (set-window-option "nowindow")
 (define make-resources '())
 
+; name is the name by which the loaded resource will be known as
+; in the rest of the source
 (define define-resource
-  (lambda (path)
-    (set! make-resources (append make-resources (list path)))))
+  (lambda (path name)
+    (set! make-resources (append make-resources (list `(,path ,name))))))
+
+(define write-schemer-build-files
+  (lambda ()
+    (with-output-to-file "build/schemer-build"
+                         (lambda ()
+                           (for-each (lambda (s)
+                                       (print (string-append (car s)
+                                                             "\t"
+                                                             (car (cdr s)))))
+                                     make-resources)))))
 
 (define make
   (lambda ()
+    (write-schemer-build-files)
     (sys '(echo "this will build the project in the future"))))
