f1b5afe84d9e6f1acb9dbc5cf02386fb3f80ad6a
krzysckh <krzysckh@wp.pl>
2023-07-10 17:01:16 +0200

    command-line.scm

--- examples/command-line.scm
+++ examples/command-line.scm
@@ -5,4 +5,8 @@
 (print "no window was spawned. now i can play with this cursed edition of scheme")
 (print "command line arguments are:")
 
-(for-each (lambda (s) (print (string-append "  - " s))) argv)
+; if the program is run as:
+; schemer command-line.scm args ...
+; argv will be set to '(schemer args ...), so it will ommit the program name, but keep argv0
+; that's why i skip the first arg here
+(for-each (lambda (s) (print (string-append "  - " s))) (cdr argv))
